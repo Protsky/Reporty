134b5486d814fd7a5b7f302bc162dd811d805f71
Gionata <donati@gionata.ch>
2023-08-20 12:24:49 +0200

    fix: design archivio web and partially on mobile

--- reporty/screens/Archivio.js
+++ reporty/screens/Archivio.js
@@ -6,149 +6,183 @@ import {
   ScrollView,
   TouchableOpacity,
 } from "react-native";
-import { AntDesign } from "@expo/vector-icons";
+import { AntDesign, Entypo } from "@expo/vector-icons"; // Import the Entypo icon
 import firebase from "firebase/compat/app";
 import "firebase/compat/database";
 
 
 
-const firebaseConfig = {
-  // Configurazione Firebase
-};
+  const firebaseConfig = {
+    // Configurazione Firebase
+  };
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
 
-const ArchivioScreen = () => {
- 
-
+  const ArchivioScreen = () => {
   
 
-  const [rapporti, setRapporti] = useState([]);
+    
 
-  useEffect(() => {
-    const attivitaRef = firebase.database().ref("attivita");
+    const [rapporti, setRapporti] = useState([]);
 
-    attivitaRef.on("value", (snapshot) => {
-      const data = snapshot.val();
-      if (data) {
-        const rapportiList = Object.entries(data).map(([key, value]) => ({
-          id: key,
-          ...value,
-        }));
-        setRapporti(rapportiList);
-      }
-    });
+    useEffect(() => {
+      const attivitaRef = firebase.database().ref("attivita");
 
-    return () => attivitaRef.off("value");
-  }, []);
+      attivitaRef.on("value", (snapshot) => {
+        const data = snapshot.val();
+        if (data) {
+          const rapportiList = Object.entries(data).map(([key, value]) => ({
+            id: key,
+            ...value,
+          }));
+          setRapporti(rapportiList);
+        }
+      });
 
-  const getWeek = (date) => {
-    const onejan = new Date(date.getFullYear(), 0, 1);
-    return Math.ceil(((date - onejan) / 86400000 + onejan.getDay() + 1) / 7);
-  };
-  const getDayOfWeek = (date) => {
-    const daysOfWeek = ["Domenica", "Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato"];
-    return daysOfWeek[date.getDay()];
-  };
+      return () => attivitaRef.off("value");
+    }, []);
 
-  const raggruppaRapportiPerSettimana = (rapporti) => {
-    const rapportiPerSettimana = {};
+    const getWeek = (date) => {
+      const onejan = new Date(date.getFullYear(), 0, 1);
+      return Math.ceil(((date - onejan) / 86400000 + onejan.getDay() + 1) / 7);
+    };
+    const getDayOfWeek = (date) => {
+      const daysOfWeek = ["Domenica", "Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato"];
+      return daysOfWeek[date.getDay()];
+    };
 
-    rapporti.forEach((rapporto) => {
-      const data = new Date(rapporto.data);
-      const settimanaKey = `${data.getFullYear()}-${getWeek(data)}`;
+    const raggruppaRapportiPerSettimana = (rapporti) => {
+      const rapportiPerSettimana = {};
 
-      if (!rapportiPerSettimana[settimanaKey]) {
-        rapportiPerSettimana[settimanaKey] = [];
-      }
-      rapportiPerSettimana[settimanaKey].push(rapporto);
-    });
+      rapporti.forEach((rapporto) => {
+        const data = new Date(rapporto.data);
+        const settimanaKey = `${data.getFullYear()}-${getWeek(data)}`;
 
-    return rapportiPerSettimana;
-  };
+        if (!rapportiPerSettimana[settimanaKey]) {
+          rapportiPerSettimana[settimanaKey] = [];
+        }
+        rapportiPerSettimana[settimanaKey].push(rapporto);
+      });
 
-  const rapportiRaggruppatiPerSettimana =
-    raggruppaRapportiPerSettimana(rapporti);
+      return rapportiPerSettimana;
+    };
 
-  const handleDeleteRapporto = (rapportoId) => {
-    // ... (existing delete code)
-  };
+    const rapportiRaggruppatiPerSettimana =
+      raggruppaRapportiPerSettimana(rapporti);
 
-  return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <Text style={styles.header}>Archivio dei rapporti:</Text>
+    const handleDeleteRapporto = (rapportoId) => {
+      // ... (existing delete code)
+    };
 
-      {Object.keys(rapportiRaggruppatiPerSettimana).map(
-        (settimanaKey, index) => (
-          <View key={index}>
-            <Text style={styles.settimanaHeader}>Settimana {settimanaKey}</Text>
-            {rapportiRaggruppatiPerSettimana[settimanaKey].map(
-              (rapporto, index) => (
-                <View style={styles.rapportoContainer} key={index}>
-                  <View>
-                    <Text style={styles.attivita}>
-                      {rapporto.attivita}
-                    </Text>
-                    <Text style={styles.orario}>
-                      {getDayOfWeek(new Date(rapporto.data))} - Orario: {rapporto.orario_inizio} - {rapporto.orario_fine}
-                    </Text>
+    
+    
+    return (
+      <ScrollView contentContainerStyle={styles.container}>
+        <Text style={styles.header}>Archivio dei rapporti:</Text>
+  
+        {Object.keys(rapportiRaggruppatiPerSettimana).map(
+          (settimanaKey, index) => (
+            <View key={index}>
+              <Text style={styles.settimanaHeader}>Settimana {settimanaKey}</Text>
+              {rapportiRaggruppatiPerSettimana[settimanaKey].map(
+                (rapporto, index) => (
+                  <View style={styles.rapportoContainer} key={index}>
+                    <Text style={styles.attivita}>{rapporto.attivita}</Text>
+                    <View style={styles.orarioDeleteContainer}>
+                      <View style={styles.dayOfWeekContainer}>
+                        <Text style={[styles.dayOfWeek, { fontWeight: "bold" }]}>
+                          {getDayOfWeek(new Date(rapporto.data))}
+                        </Text>
+                      </View>
+                      <Text style={styles.orario}>
+                        {rapporto.orario_inizio} - {rapporto.orario_fine}
+                      </Text>
+                      <TouchableOpacity
+                        style={styles.deleteButton}
+                        onPress={() => handleDeleteRapporto(rapporto.id)}
+                      >
+                        <AntDesign name="delete" size={24} color="red" />
+                      </TouchableOpacity>
+                      <TouchableOpacity
+                        style={styles.downloadButton}
+                        onPress={() => {
+                          // Implement the download logic here
+                        }}
+                      >
+                        <Entypo name="download" size={24} color="blue" />
+                      </TouchableOpacity>
+                    </View>
                   </View>
-                  <TouchableOpacity
-                    onPress={() => handleDeleteRapporto(rapporto.id)}
-                  >
-                    <AntDesign name="delete" size={24} color="red" />
-                  </TouchableOpacity>
-                </View>
-              )
-            )}
-          </View>
-        )
-      )}
-    </ScrollView>
-  );
-};
+                )
+              )}
+            </View>
+          )
+        )}
+      </ScrollView>
+    );
+  };
 
-const styles = StyleSheet.create({
-  ontainer: {
-    padding: 16,
-    backgroundColor: "#f0f0f0",
-  },
-  header: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 16,
-  },
-  settimanaHeader: {
-    fontSize: 15,
-    fontWeight: "bold",
-    marginTop: 16,
-    marginBottom:10
-  },
-  rapportoContainer: {
-    backgroundColor: "#fff",
-    borderRadius: 8,
-    padding: 8, // Adjusted padding
-    marginBottom: 8, // Adjusted margin
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 4,
-    elevation: 3,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  attivita: {
-    fontSize: 16, // Adjusted font size
-    fontWeight: "bold",
-    marginBottom: 8, // Adjusted margin
-  },
-  orario: {
-    fontSize: 14, // Adjusted font size
-    color: "#888",
-  },
-});
+  const styles = StyleSheet.create({
+    container: {
+      padding: 2,
+      backgroundColor: "#f0f0f0",
+    },
+    dayOfWeekContainer: {
+      width: 80, // Set a fixed width for the day of the week
+    },
+    dayOfWeek: {
+      fontSize: 14,
+      color: "#888",
+      fontWeight: "bold",
+    },
+    header: {
+      fontSize: 24,
+      fontWeight: "bold",
+      marginBottom: 16,
+    },
+    settimanaHeader: {
+      fontSize: 15,
+      fontWeight: "bold",
+      marginTop: 16,
+      marginBottom:10
+    },
+    deleteButton: {
+      marginLeft: 10, // Add margin to separate delete icon from time text
+    },
+    rapportoContainer: {
+      backgroundColor: "#fff",
+      borderRadius: 8,
+      padding: 8, // Adjusted padding
+      marginBottom: 8, // Adjusted margin
+      shadowColor: "#000",
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.2,
+      shadowRadius: 4,
+      elevation: 3,
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-between",
+    },
+    orarioDeleteContainer: {
+      flexDirection: "row",
+      alignItems: "center", // Align items at the center of the container
+      justifyContent: "flex-start", // Start the content from the left
+    },
+    attivita: {
+      fontSize: 16, // Adjusted font size
+      fontWeight: "bold",
+      marginBottom: 8, // Adjusted margin
+      marginRight: 8,
+    },
+    orario: {
+      fontSize: 14, // Adjusted font size
+      color: "#888",
+    },
+    downloadButton: {
+      marginLeft: 5, // Add margin to separate download icon from delete icon
+    },
+  });
 
-export default ArchivioScreen;
+  export default ArchivioScreen;
